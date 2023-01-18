13c14ed32f547ac3f68f2476c4c7f8291e03dd5a
Richard Pascual <ricpascual@gmail.com>
2023-01-18 18:58:28 -0500

    fundamentos de react

--- src/App.js
+++ src/App.js
@@ -7,22 +7,69 @@ import { TodoButtom } from "./TodoButtom.js";
 
 
 
-const todos = [
-  {text:'Cortar cebolla', completed:false},
-  {text:'Tormar el curso de intro a react', completed:false},
-  {text:'Llorar con la llorona', completed:false}
+const defaultodos = [
+  {text:'Cortar cebolla', completed:true},
+  {text:'Tormar el curso de intro a react', completed:true},
+  {text:'Llorar con la llorona', completed:false},
+  {text:'como esamos', completed:false}
 ];
 
 function App() {
+
+  const [todos, setTodos] = React.useState(defaultodos);
+  const [searchValue, setSearchaValue] = React.useState("");
+
+  const completedTodos = todos.filter(todo => todo.completed===true).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+  if (!searchValue.length >= 1){
+    searchedTodos = todos;
+  }else{
+    searchedTodos = todos.filter(todo =>{
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText)
+    })
+  }
+
+  
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    setTodos(newTodos);
+  }
+
+
+  const deleteTodo = (text)=>{
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+
+  }
+
   return (
     <div className='bg-gray-100 p-2'>
-      
     <div className='border-solid border-2  border-indigo-600  rounded-[0.5rem] w-full mx-auto p-4 bg-white'>
-      <TodoCounter/>
-      <TodoSearch/>
+      
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
+
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchaValue}/>
       <TodoList>
-        {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text}/>
+        {searchedTodos.map(todo => (
+          <TodoItem 
+           key={todo.text} 
+           text={todo.text}
+           completed={todo.completed}
+           onCompleted={() => completeTodo(todo.text)}
+           onDeleted={() => deleteTodo(todo.text)}
+          />
         ))}
 
       </TodoList>
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BsCalendarCheck } from "react-icons/bs"
-function TodoCounter(){
+function TodoCounter({total, completed}){
     return (
     
         <div className="pt-5">
@@ -11,7 +11,7 @@ function TodoCounter(){
             
             <p>your dayly progress will shown here</p>
             
-            <h2 className="font-serif text-center pt-5"> Has completado 2 de 3 ToDos </h2>
+            <h2 className="font-serif text-center pt-5"> Has completado {completed} de {total} ToDos </h2>
 
 
         </div>
